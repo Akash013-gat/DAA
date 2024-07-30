165f520e884266b3c89836f9ce2ddb6c8b653928
Akash013-gat <akashdat2004@gmail.com>
2024-07-30 15:36:52 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,37 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define size 100
-int c;
-void bubblesort(int a[size],int n)
+void Prims(int c[10][10],int n)
 {
-    int i,j,t;
-    for(i=0;i<=n-2;i++)
+    int i,j,v[10],ne=1,u,v1,mc=0,min;
+    for(i=1;i<=n;i++)
+        v[i]=0;
+    v[1]=1;
+    printf("\n Edge considered are \n");
+    while(ne<n)
     {
-        for(j=0;j<=n-2-i;j++)
+        min=999;
+        for(i=1;i<=n;i++)
         {
-
-        c++;
-        if(a[j]>a[j+1])
-        {
-            t=a[j];
-            a[j]=a[j+1];
-            a[j+1]=t;
+            for(j=1;j<=n;j++)
+            {
+              if(c[i][j]<min){
+              if(v[i]==1)
+                 continue;
+              else{
+                min=c[i][j];
+                u=i;
+                v1=j;
+              }
+            }
         }
         }
+   if(v[u]==0 || v[v1]==0)
+   {
+        printf("\n %d  %d=%d",ne++,u,v1,min);
+        mc=mc+min;
+        v[v1]=1;
+   }
+   c[u][v1]=c[v1][u]=999;
     }
+     printf("\nCost Of Spanning tree is %d",mc);
 }
 int main()
 {
-    int i,a[size],n;
-    printf("read Array :\n");
+    int n,c[10][10],i,j;
+    printf("\nRead number of nodes :");
     scanf("%d",&n);
-    printf("\n read array element\n");
-    for(i=0;i<n;i++)
-        scanf("%d",&a[i]);
-    bubblesort(a,n);
-    printf("\n Sorted element are \n");
-    for(i=0;i<n;i++)
-        printf("%d\t",a[i]);
-        printf("comparsion=%d\n",c);
+     printf("\n Read cost matrix \n");
+     for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            scanf("%d",&c[i][j]);
+            if(c[i][j]==0)
+                c[i][j]=999;
+        }
+     }
+     Prims(c,n);
     return 0;
 }
