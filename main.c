704c15f7159934045d20cd6e09ab4f4dcc834ebb
Akash013-gat <akashdat2004@gmail.com>
2024-07-30 15:35:20 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,37 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define size 100
-int c;
-void bubblesort(int a[size],int n)
+int max(int a,int b)
 {
-    int i,j,t;
-    for(i=0;i<=n-2;i++)
-    {
-        for(j=0;j<=n-2-i;j++)
-        {
 
-        c++;
-        if(a[j]>a[j+1])
-        {
-            t=a[j];
-            a[j]=a[j+1];
-            a[j+1]=t;
-        }
-        }
-    }
+    if(a>b)
+        return a;
+    else
+        return b;
 }
 int main()
 {
-    int i,a[size],n;
-    printf("read Array :\n");
+    int n,w[10],v[10],V[10][10],W,x[10]={0},i,j;
+    printf("\n Read number of objects :\n");
     scanf("%d",&n);
-    printf("\n read array element\n");
-    for(i=0;i<n;i++)
-        scanf("%d",&a[i]);
-    bubblesort(a,n);
-    printf("\n Sorted element are \n");
-    for(i=0;i<n;i++)
-        printf("%d\t",a[i]);
-        printf("comparsion=%d\n",c);
+    printf("\n Read weights of the objects\n");
+    for(i=1;i<=n;i++)
+        scanf("%d",&w[i]);
+    printf("\n Read profits of the objects\n");
+    for(i=1;i<=n;i++)
+        scanf("%d",&v[i]);
+    printf("\n Read knapsack capacity:");
+    scanf("%d",&W);
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=W;j++){
+            if(i==0 || j==0)
+                V[i][j]=0;
+            else if(j<w[i])
+               V[i][j]=V[i-1][j];
+            else
+                V[i][j]=max(V[i-1][j],V[i-1][j-w[i]]+v[i]);
+            printf("%d\t",V[i][j]);
+            }
+            printf("\n");
+    }
+    printf("\n max profit is %d",V[n][W]);
+    i=n;
+    j=W;
+    while(i!=0 && j!=0)
+    {
+        if(V[i][j]!=V[i-1][j])
+        {
+            x[i]=1;
+            j=j-w[i];
+        }
+        i=i-1;
+    }
+    printf("\n Objects included are \n");
+    for(i=1;i<=n;i++){
+        if(x[i]==1)
+            printf("\n %d objects is included",i);
+    }
     return 0;
 }
